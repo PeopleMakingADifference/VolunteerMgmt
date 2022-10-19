998d0280e7e498f843af015deb56630a3900e4e2
Paul Couto <paul@pepper.me>
2022-10-18 20:34:47 -0400

    First pass as new push notification support - needs more setup to test

--- MobileFrontendV2/src/app/push.service.ts
+++ MobileFrontendV2/src/app/push.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { User } from './user.service';
-//import {FCM} from '@ionic-native/fcm';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
+
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
 
 // allows the app to receive push notifications
 
@@ -19,11 +25,14 @@ export class PushService {
   ){
     this.platform.ready().then(()=>{
       if(this.platform.is('android')){
-        //fcm.onNotification().subscribe(data => {
-        //  if(data.intent && data.intent === 'checkout_reminder'){
-        //    this.router.navigate(['/check-out-reminded']);
-        //  }
-        //});
+        // Show us the notification payload if the app is open on our device
+        PushNotifications.addListener('pushNotificationReceived',
+          (notification: PushNotificationSchema) => {
+          if (notification.data.intent && notification.data.intent === 'checkout_reminder'){
+            console.log('Push received: ' + JSON.stringify(notification));
+            this.router.navigate(['/check-out-reminded']);
+          }
+        });
       }
     });
   }
@@ -31,16 +40,37 @@ export class PushService {
   register(user: User, endpoint: string): Promise<any>{
     return new Promise((resolve, reject) => {
       this.platform.ready().then(()=>{
-        //this.fcm.getToken().then(token => {
-        //  this.sendToken(endpoint, token, user.id)
-        //  .then(response => resolve(response))
-        //  .catch(err => reject(err));
-        //});
-        //this.fcm.onTokenRefresh().subscribe(token => {
-        //  this.sendToken(endpoint, token, user.id)
-        //  .then(response => console.log('refreshed token.'))
-        //  .catch(err => console.error('error refreshing token', err));
-        //});
+        // Request permission to use push notifications
+        // iOS will prompt user and return if they granted permission or not
+        // Android will just grant without prompting
+        PushNotifications.requestPermissions().then(result => {
+          if (result.receive === 'granted') {
+            // Register with Apple / Google to receive push via APNS/FCM
+            PushNotifications.register();
+          } else {
+            console.log('Push permission request failed: ' + JSON.stringify(result));
+          }
+        });
+
+        // On success, we should be able to receive notifications
+        PushNotifications.addListener('registration', (token: Token) => {
+          console.log('Push registration success, token: ' + token.value);
+          this.sendToken(endpoint, token.value, user.id)
+          .then(response => resolve(response))
+          .catch(err => reject(err));
+        });
+
+        // Some issue with our setup and push will not work
+        PushNotifications.addListener('registrationError', (error: any) => {
+          console.log('Error on registration: ' + JSON.stringify(error));
+          reject(error);
+        });
+
+        // Method called when tapping on a notification
+        PushNotifications.addListener('pushNotificationActionPerformed',
+          (notification: ActionPerformed) => {
+          console.log('Push action performed: ' + JSON.stringify(notification));
+        });
       });
     });
   }
