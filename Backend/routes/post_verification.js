c029a3bda2b3b3d4f342f58061524359ee538273
Paul Couto <pcouto64@gmail.com>
2023-10-21 19:11:36 -0400

    Remove hard-coded verification code now that Twilio is working again

--- Backend/routes/post_verification.js
+++ Backend/routes/post_verification.js
@@ -15,7 +15,7 @@ module.exports = function(app, dbconn) {
       ).toArray((err, items) => {
         if (items.length === 1) {
           for (let v of items[0].volunteers) {
-            if ((v.verif_code === req.body.verif_code) || (req.body.verif_code === '123456')) {
+            if (v.verif_code === req.body.verif_code) {
               res.send({'response': 'Ok, you\'re verified.'});
               return;
             }
